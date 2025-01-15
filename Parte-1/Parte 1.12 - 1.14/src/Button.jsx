732e7b67047c4c5a93df374c9ed44c18f89a441e
Angelyn Bonaldy <anggi@Angelyns-iMac.local>
2025-01-15 17:50:59 +0100

    Button to generate random phrase

---
+++ Parte-1/Parte 1.12 - 1.14/src/Button.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+const Button = ({ text, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="p-2 m-2 bg-blue-200 rounded-md cursor-pointer max-w-fit"
+    >
+      {text}
+    </div>
+  );
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Button;
