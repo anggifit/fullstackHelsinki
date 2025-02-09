cc01b0b9b6180bb81544c7571f01bb1d1c0f6c1f
Angelyn Bonaldy <anggi@Angelyns-iMac.local>
2025-02-09 11:24:48 +0100

    Initializing example backend app

--- Parte-3/index.js
+++ Parte-3/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json()); // Middleware to parse the body of the request as JSON
+
 let notes = [
   {
     id: 1,
@@ -18,6 +20,8 @@ let notes = [
     important: true,
   },
 ];
+
+const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0; // Get the maximum id of the notes
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>"); // Send a response to the client, stablishing the content type as text/HTML
@@ -38,6 +42,23 @@ app.delete("/api/notes/:id", (req, res) => {
   const id = Number(req.params.id);
   notes = notes.filter((note) => note.id !== id);
   res.status(204).end();
+});
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    id: maxId + 1,
+    content: body.content,
+    important: body.important || false,
+  };
+  notes = notes.concat(note);
+  res.json(note);
 });
 
 const PORT = 3001;
