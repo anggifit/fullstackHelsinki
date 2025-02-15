ce52e7331304ff45fc7e6381e867f6682c88450d
Angelyn Bonaldy <anggi@Angelyns-iMac.local>
2025-02-15 09:53:07 +0100

    Add validators to schema

--- Parte-3/index.js
+++ Parte-3/index.js
@@ -42,35 +42,31 @@ app.delete("/api/notes/:id", (request, response, next) => {
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => response.json(updatedNote))
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content missing" });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.status(201).json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.status(201).json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 // Middleware para manejar rutas no encontradas
@@ -85,7 +81,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
+
   next(error);
 };
 
